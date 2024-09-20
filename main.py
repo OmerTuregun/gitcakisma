d46ba884fef85e306706e68288c4445ef4782e88
OmerTuregun <trkuraf@gmail.com>
2024-09-20 12:25:57 +0300

    aldım degisikligi

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ def yeni_fonksiyon1():
 
 def yeni_fonksiyon2():
     print("A kisisi tarafindan eklenen fonksiyon2.")
+
+from helper import yeni_fonksiyon3
+
+yeni_fonksiyon3()
+print("B kişisi burada calisiyor3.")
