e0502379055c0e42b2fe6d15be36f27c1f469123
OmerTuregun <trkuraf@gmail.com>
2024-09-20 12:19:01 +0300

    B kişisinin değişiklikleri

--- helper.py
+++ helper.py
@@ -2,3 +2,14 @@ from main import yeni_fonksiyon
 
 yeni_fonksiyon()
 print("B kişisi burada calisiyor.")
+
+from main import yeni_fonksiyon1
+
+
+yeni_fonksiyon1()
+print("B kişisi burada calisiyor1.")
+
+from main import yeni_fonksiyon2
+
+yeni_fonksiyon2()
+print("B kişisi burada calisiyor2.")
